6c140fe3e2e93c863b50f1a9aafb3e4abb8335f9
Nikola Milijevic <nikolamilijevic5@gmail.com>
2025-09-27 14:38:55 +0200

    AdminDashboard button fix

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -7,7 +7,7 @@ import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 
 const Topbar = () => {
-    const isAdmin = useAuthStore();
+    const {isAdmin} = useAuthStore();
     return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
         <div className="flex gap-2 items-center">
